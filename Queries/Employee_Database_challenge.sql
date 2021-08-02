acfaec48d127723992d44849b79b52cdd00e15c5
Soco Hernandez <85274353+chocoplace@users.noreply.github.com>
2021-08-02 04:13:58 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -38,5 +38,26 @@ ORDER BY COUNT (title) DESC;
 --Check table
 SELECT * FROM retiring_titles;
 
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+-- From requirements 1-11
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+-- Check table
+SELECT * FROM mentorship_eligibility;
+
 
 
